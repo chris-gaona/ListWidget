f6d8ce4c193062bf7ed6e603af0e73118be2c3f9
Chris Gaona <thebeloved88@sbcglobal.net>
2017-12-21 09:09:28 -0800

    Updates the widget to receive clicks using PendingIntent

--- app/src/main/java/com/chrisgaona/listwidget/WidgetProvider.java
+++ app/src/main/java/com/chrisgaona/listwidget/WidgetProvider.java
@@ -1,8 +1,10 @@
 package com.chrisgaona.listwidget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 /**
@@ -15,6 +17,19 @@ public class WidgetProvider extends AppWidgetProvider {
         // app widget provider is a broadcast receiver
         for (int id : appWidgetIds) {
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+
+            Intent intent = new Intent(context, WidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
+            // pending intent
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                    context,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.frameLayout, pendingIntent);
+
             appWidgetManager.updateAppWidget(id, remoteViews);
         }
     }
