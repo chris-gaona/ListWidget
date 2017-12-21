eca6e91094e8900072beebf3d3d7985e74660ade
Chris Gaona <thebeloved88@sbcglobal.net>
2017-12-21 09:45:50 -0800

    Finishes up the widget

--- app/src/main/java/com/chrisgaona/listwidget/WidgetProvider.java
+++ app/src/main/java/com/chrisgaona/listwidget/WidgetProvider.java
@@ -7,6 +7,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 /**
  * Created by chrisgaona on 12/21/17.
@@ -14,6 +15,20 @@ import android.widget.RemoteViews;
 
 public class WidgetProvider extends AppWidgetProvider {
     public static final String KEY_ITEM = "com.chrisgaona.listwidget.KEY_ITEM";
+    public static final String TOAST_ACTION = "com.chrisgaona.listwidget.TOAST_ACTION";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(TOAST_ACTION)) {
+            String listItem = intent.getStringExtra(KEY_ITEM);
+            Toast.makeText(context, listItem, Toast.LENGTH_SHORT).show();
+//            onUpdate(context, AppWidgetManager.getInstance(context), null);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        }
+
+        super.onReceive(context, intent);
+    }
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         int[] realAppWidgetIds = AppWidgetManager.getInstance(context).getAppWidgetIds(new ComponentName(context, WidgetProvider.class));
@@ -32,7 +47,7 @@ public class WidgetProvider extends AppWidgetProvider {
             remoteViews.setInt(R.id.frameLayout, "setBackgroundColor", color);
 
             Intent intent = new Intent(context, WidgetProvider.class);
-            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.setAction(TOAST_ACTION);
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, realAppWidgetIds);
 
             // pending intent
